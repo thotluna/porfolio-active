746ac1bf17df0315b239b01ab4d18fe152d9c485
Eladio Feijoo <eladio.feijoo@gmal.com>
2022-11-27 01:36:29 -0400

    fix(aparagraph): fix write error

--- src/components/my-history/AParagraph.jsx
+++ src/components/my-history/AParagraph.jsx
@@ -8,7 +8,7 @@ export default function AParagraph() {
 
   return $isCollapseParagraph && <div className={styles.aParagraph}>
     <p>
-      Desde pequeño, mi padre me llevaba a su trabajo y aunque no entendía nada, igual quedaba hipnotizado por toda la tecnología. A los 14 años empece a trabajar en la empresa de mi padre como un simple obrero, y hay entendí que lo que me atraía de la tecnología era la posibilidad de crear.
+      Desde pequeño, mi padre me llevaba a su trabajo y aunque no entendía nada, igual quedaba hipnotizado por toda la tecnología. A los 14 años empece a trabajar en la empresa de mi padre como un simple obrero, y hay entendí que lo que me atraía de la tecnología era la posibilidad de crear nuevas cosas.
     </p>
   </div>
 
